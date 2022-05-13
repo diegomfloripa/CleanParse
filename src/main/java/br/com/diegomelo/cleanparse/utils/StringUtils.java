ad59214a05556087eb72b8a0c34a6414406d613e
Diego Melo <diegomelodev@gmail.com>
2022-05-13 10:07:21 -0300

    test

--- src/main/java/br/com/diegomelo/cleanparse/utils/StringUtils.java
+++ src/main/java/br/com/diegomelo/cleanparse/utils/StringUtils.java
@@ -16,6 +16,10 @@ public final class StringUtils {
 		if(value == null || value.isEmpty()) return value;
 		String charLowerCase = String.valueOf(value.charAt(0));
 		String charUpperrCase = charLowerCase.toUpperCase();
+		//TODO somente um todo novo para ser criitcado pelo sonar
+		if(1=1) {
+			System.out.println("1");
+		}
 		return value.replaceFirst(charLowerCase, charUpperrCase);
 	}
 }
